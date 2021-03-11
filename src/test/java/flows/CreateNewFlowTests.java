0925da2cbbcef48af2be4d086ca711359dab32be
sihan-yu <imccoy17@icloud.com>
2021-03-11 16:50:12 +0000

    read file from CSV file

--- src/test/java/flows/CreateNewFlowTests.java
+++ src/test/java/flows/CreateNewFlowTests.java
@@ -1,15 +1,28 @@
 package flows;
 
 import base.BaseTests;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import pages.FlowsPage;
+import utils.TestUtil;
 
 public class CreateNewFlowTests extends BaseTests {
+
+    private String sheetName = "contacts";
+
+
+    @DataProvider(name = "flowName-provider")
+    public Object [][]  flowData ()
+    {
+        Object data[][] = TestUtil.getTestData(sheetName);
+        return data;
+    }
+
     @Test
     public void testClickCreateNewFlow(){
         var flowsPage = dashboardPage.clickFlows();
-        flowsPage.clickCreateNewFlow();
+        flowsPage.clickCreateNewFlow(); // issue here: click some button which is not actually we want
         // flowsPage.setFlowName("test");
         // flowsPage.clickNextButton();
     }
+
 }
